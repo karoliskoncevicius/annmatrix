d3a9dced31050d5dcdfaf1c89342d3f1db7b2f51
KarolisKoncevicius <karolis.koncevicius@gmail.com>
2018-08-27 21:40:47 +0300

    removed Math group generics as it works better without it (log2, log10, etc work correctly)

--- R/groupgenerics.R
+++ R/groupgenerics.R
@@ -1,11 +1,6 @@
 #' @export
-Math.annMatrix <- function(x, ...) {
-  annMatrix(NextMethod(), rowAnn=attr(x, "rowAnn"), colAnn=attr(x, "colAnn"))
-}
-
-#' @export
-Ops.annMatrix <- function(x, ...) {
-  annMatrix(NextMethod(), rowAnn=attr(x, "rowAnn"), colAnn=attr(x, "colAnn"))
+Ops.annMatrix <- function(e1, e2) {
+  annMatrix(NextMethod(), rowAnn=attr(e1, "rowAnn"), colAnn=attr(e1, "colAnn"))
 }
 
 
