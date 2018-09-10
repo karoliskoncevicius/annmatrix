2d8e84ac4805a083279d9d2fe9b85a1d060eaf52
KarolisKoncevicius <karolis.koncevicius@gmail.com>
2018-09-10 21:46:05 +0300

    simplified rowsplit and colsplit functions

--- R/split.R
+++ R/split.R
@@ -8,7 +8,6 @@
 #' @param x an R object. Typically a \code{matrix} or \code{data.frame} to be divided into groups.
 #' @param group grouping variable for rows or columns used for dividing an object into groups.
 #' Can also be a list of groups in which case their interaction is used for grouping.
-#' @param ... further arguments passed to or from methods.
 #'
 #' @return transposed annmatrix object
 #'
@@ -21,39 +20,13 @@
 #'
 #' @author Karolis Koncevičius
 #' @export
-rowsplit <- function(x, group, ...) {
-  UseMethod("rowsplit")
+rowsplit <- function(x, group) {
+  lapply(split(seq_len(nrow(x)), group), function(ind) x[ind, , drop=FALSE])
 }
 
 #' @rdname split
 #' @export
-colsplit <- function(x, group, ...) {
-  UseMethod("colsplit")
-}
-
-#' @rdname split
-#' @export
-rowsplit.default <- function(x, group, ...) {
-  x <- as.matrix(x)
-  split.data.frame(x, group)
-}
-
-#' @rdname split
-#' @export
-colsplit.default <- function(x, group, ...) {
-  x <- as.matrix(x)
-  Map(t, split.data.frame(t(x), group))
-}
-
-#' @rdname split
-#' @export
-rowsplit.data.frame <- function(x, group, ...) {
-  split.data.frame(x, group)
-}
-
-#' @rdname split
-#' @export
-colsplit.data.frame <- function(x, group, ...) {
-  split.default(x, group)
+colsplit <- function(x, group) {
+  lapply(split(seq_len(ncol(x)), group), function(ind) x[, ind, drop=FALSE])
 }
 
