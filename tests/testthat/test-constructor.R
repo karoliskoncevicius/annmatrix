9b6d4d9cb59b140dd2b467ffa90a427d0ca75e8a
KarolisKoncevicius <karolis.koncevicius@gmail.com>
2018-09-19 00:19:36 +0300

    added tests for transpose

--- tests/testthat/test-constructor.R
+++ tests/testthat/test-constructor.R
@@ -1,6 +1,12 @@
 context("constructor")
 
 test_that("empty constructor works as expected", {
-  mat <- annmatrix()
-  expect_equal(dim(mat), c(0,0))
+  mat  <- matrix(nrow=0, ncol=0)
+  df   <- data.frame(row.names=NULL)
+  amat <- annmatrix()
+  expect_equal(as.matrix(amat), mat)
+  expect_equal(colanns(amat), df)
+  expect_equal(rowanns(amat), df)
+  amat2 <- annmatrix(mat, df, df)
+  expect_equal(amat, amat2)
 })
