b47d85bae7cf94226e142aeb85a820580d49c3b3
KarolisKoncevicius <karolis.koncevicius@gmail.com>
2019-02-27 23:24:39 +0200

    added annmat2list

--- R/transform.R
+++ R/transform.R
@@ -56,11 +56,39 @@ t.annmatrix <- function(x) {
 annmat2long <- function(x, ...) {
   rann <- attr(x, ".annmatrix.rann")
   cann <- attr(x, ".annmatrix.cann")
-  longdf <- data.frame(value=as.numeric(x),
-                       rann[rep(seq_len(nrow(rann)), nrow(cann)), , drop=FALSE],
-                       cann[rep(seq_len(nrow(cann)), each=nrow(rann)), , drop=FALSE],
-                       ...
-                       )
-  longdf
+  data.frame(value=as.numeric(x),
+             rann[rep(seq_len(nrow(rann)), nrow(cann)), , drop=FALSE],
+             cann[rep(seq_len(nrow(cann)), each=nrow(rann)), , drop=FALSE],
+             ...
+             )
+}
+
+#' Transform annmatrix Object to List Format
+#'
+#' Turns the matrix and it's column and row meta-data into a \code{list}.
+#'
+#' @param x annmatrix object
+#' @param ... other parameters passed to \code{list}
+#'
+#' @return a \code{list}
+#'
+#' @examples
+#' # construct the annmatrix object
+#' coldata <- data.frame(group=c(rep("case", 20), rep("control", 20)),
+#'                       gender=sample(c("M", "F"), 40, replace=TRUE)
+#'                       )
+#' rowdata <- data.frame(chr=sample(c("chr1", "chr2"), 100, replace=TRUE),
+#'                          pos=runif(100, 0, 1000000)
+#'                          )
+#' annMat <- annmatrix(matrix(rnorm(100*40), 100, 40), rowdata, coldata)
+#'
+#' annmat2list(annMat)
+#'
+#' @author Karolis Koncevičius
+#' @export
+annmat2list <- function(x, ...) {
+  rann <- attr(x, ".annmatrix.rann")
+  cann <- attr(x, ".annmatrix.cann")
+  list(mat=as.matrix(x), rowanns=rann, colanns=cann, ...)
 }
 
