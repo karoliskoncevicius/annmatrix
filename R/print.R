4d407362f332faea9f7e9651306620ca90db0921
KarolisKoncevicius <karolis.koncevicius@gmail.com>
2019-05-14 03:18:47 +0300

    improved printing method

--- R/print.R
+++ R/print.R
@@ -30,23 +30,46 @@
 print.annmatrix <- function(x, nrow=5, ncol=5, ...) {
   rows <- nrow(x)
   cols <- ncol(x)
-  if(rows > 0 & cols > 0) {
-    mat <- format(x[seq_len(min(nrow, rows)), seq_len(min(ncol, cols)), drop=FALSE], digits=5)
-    if(rows > nrow) {
-      mat <- rbind(mat, "...")
-    }
-    if(cols > ncol) {
-      mat <- cbind(mat, "...")
-    }
-    print.default(format(mat, justify="right"), quote=FALSE)
-    cat("\n")
+  rinds <- seq_len(min(nrow, rows))
+  cinds <- seq_len(min(ncol, cols))
+
+  if(rows > nrow) rinds <- c(head(rinds, -1), NA, rows)
+  if(cols > ncol) cinds <- c(head(cinds, -1), NA, cols)
+
+  mat  <- as.matrix(x[rinds, cinds, drop=FALSE])
+  mat  <- format(mat)
+  fill <- paste(rep(".", max(0, nchar(mat))), collapse="")
+  mat[is.na(rinds),] <- fill
+  mat[,is.na(cinds)] <- fill
+
+  rnames <- rownames(mat)
+  cnames <- colnames(mat)
+  if(is.null(rnames) & rows > 0) {
+    rnames <- paste0("[", rinds, ",]")
+    rownames(mat) <- format(rnames, justify="right")
   }
-  cat("rows:    ", rows, " ", sep="")
+  if(is.null(cnames) & cols > 0) {
+    cnames <- paste0("[,", cinds, "]")
+    cgap   <- max(0, max(0, nchar(mat)) - max(0, nchar(cnames)))
+    cnames <- paste0(paste(rep(" ", cgap), collapse=""), cnames)
+    colnames(mat) <- format(cnames, justify="right")
+  }
+
+  rownames(mat)[is.na(rinds)] <- ""
+  colnames(mat)[is.na(cinds)] <- ""
+
+
+  print.default(mat, justify="left", quote=FALSE)
+  cat("\n")
+
+  cat("rann: ")
   cat(names(attr(x, ".annmatrix.rann")), sep=", ")
   cat("\n")
-  cat("columns: ", cols, " ", sep="")
+
+  cat("cann: ")
   cat(names(attr(x, ".annmatrix.cann")), sep=", ")
   cat("\n")
+
   invisible(x)
 }
 
